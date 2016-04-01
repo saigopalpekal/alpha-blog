b733cacf321d23782b68ef6086fd6f487c6fda9f
Sai Rails <psaigopal1988@gmail.com>
2016-04-01 12:19:12 +0000

    Create edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def edit
+  
+    @article=Article.find(params[:id])
+    
+  end
+  
+  
+  
   def create
   
    
@@ -14,6 +22,20 @@ class ArticlesController < ApplicationController
       else
         render 'new'
       end
+  
+  end
+  
+  def update
+    
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+    
+      flash[:notice]="Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+      
   
   end
   
